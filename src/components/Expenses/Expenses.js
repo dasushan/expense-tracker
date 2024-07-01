7c674d0703abdc0f914876c3f2f850846a035e9a
dasushan <53913896+dasushan@users.noreply.github.com>
2024-07-01 12:28:44 +0530

    classifying components into sub-folders

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,13 +1,13 @@
 import './Expenses.css';
 import ExpenseItem from './ExpenseItem';
-import Card from "./Card"
+import Card from '../UI/Card';
 
 function Expenses({ expenses }) {
   return (
     <Card className="expenses">
       {expenses.map((expense) => {
         return (
-          <ExpenseItem 
+          <ExpenseItem
             key={expense.id}
             date={expense.date}
             title={expense.title}
