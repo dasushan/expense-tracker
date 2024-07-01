157307e331df96f2a76a555e8680c20493a49839
dasushan <53913896+dasushan@users.noreply.github.com>
2024-07-01 13:29:48 +0530

    adding state to components

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,15 +1,18 @@
+import React, {useState} from "react";
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
+
 function ExpenseItem(props) {
+  const [title, setTitle] = useState(props.title)
   const buttonClickHandler = () => {
-    console.log('Button Clicked');
+    setTitle("New Title");
   }
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.price}</div>
       </div>
       <div className="expense-item__location">{props.location}</div>
