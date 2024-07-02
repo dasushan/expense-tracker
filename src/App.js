15be6ba37d3d52243b10f0ff401f36169aec5a8c
dasushan <53913896+dasushan@users.noreply.github.com>
2024-07-02 07:58:42 +0530

    adding form-component in the folder structure

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import Expenses from './components/Expenses/Expenses';
+import NewExpense from './components/NewExpense/NewExpense';
+
 function App() {
   const expenses = [
     {
@@ -32,7 +34,7 @@ function App() {
   ];
   return (
     <div>
-      <h1>Let's get started</h1>
+      <NewExpense />
       <Expenses expenses={expenses} />
     </div>
   );
