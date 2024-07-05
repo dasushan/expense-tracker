82145fc10a6133e8607db6447c99a5a85d962409
dasushan <53913896+dasushan@users.noreply.github.com>
2024-07-05 23:14:57 +0530

    conditional rendering expenses-item

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,32 +1,40 @@
-import React, {useState} from "react";
+import React, { useState } from 'react';
 import './Expenses.css';
 import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
-import ExpensesFilter from "./ExpensesFilter"
+import ExpensesFilter from './ExpensesFilter';
 
 function Expenses({ expenses }) {
   const [filteredYear, setFilteredYear] = useState('2023');
   const changeFilterHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
-  }
+  };
 
-  const filteredExpenses = expenses.filter((expense)=>{
-    return expense.date.getFullYear().toString() == filteredYear
-  })
-  
+  const filteredExpenses = expenses.filter((expense) => {
+    return expense.date.getFullYear().toString() == filteredYear;
+  });
+
+  let expensesContent = <p>No expenses found</p>
+
+  if(filteredExpenses.length >= 1){
+    expensesContent = filteredExpenses.map((expense) => {
+      return (
+        <ExpenseItem 
+          key={expense.id}
+          date={expense.date}
+          price={expense.price}/>
+      )
+    })
+  }
   return (
     <Card className="expenses">
-      <ExpensesFilter selected={filteredYear} onChangeFilter={changeFilterHandler}/>
-      {filteredExpenses.map((expense) => {
-        return (
-          <ExpenseItem
-            key={expense.id}
-            date={expense.date}
-            title={expense.title}
-            price={expense.price}
-          />
-        );
-      })}
+      <ExpensesFilter
+        selected={filteredYear}
+        onChangeFilter={changeFilterHandler}
+      />
+      
+      {expensesContent}
+      { filteredExpenses.length ==1 ? <p>Only one expense found. Please add more </p> : ""}
     </Card>
   );
 }
